38f9c54c9f60dab93b9510783ff81af47f350df4
sielee <tldms975@naver.com>
2023-04-09 03:21:35 +0900

    ~

--- srcs/requirements/mariadb/tools/init_db.sql
+++
@@ -1,9 +0,0 @@
--- create database
-CREATE DATABASE wordpress;
-
--- create user and grant permissions
-CREATE USER 'wordpress_user'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress_user'@'%';
-
--- flush privileges
-FLUSH PRIVILEGES;
