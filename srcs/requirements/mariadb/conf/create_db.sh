52355ae0603f702791d8276a188efa0a83ac876b
sielee <tldms975@naver.com>
2023-04-23 13:11:33 +0900

    struggling

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -5,7 +5,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         chown -R mysql:mysql /var/lib/mysql
 
         # init database
-        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=myuser --rpm
 
         tfile=`mktemp`
         if [ ! -f "$tfile" ]; then
