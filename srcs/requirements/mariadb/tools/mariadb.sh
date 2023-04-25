7aa387a434d6670e31868cf25ed6ecb5d2dd7f50
sielee <lalala97557@gmail.com>
2023-04-25 16:29:15 +0900

    running

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,19 +1,42 @@
-mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+#!/bin/sh
 
-/usr/bin/mysqld --user=mysql --bootstrap << EOF
-use mysql;
-flush privileges;
+mysql_install_db
 
-alter user 'root'@'localhost' identified by '$DB_ROOT_PASSWORD';
-flush privileges;
+/etc/init.d/mysql start
 
-create database $DB_NAME;
-create user '$DB_USER'@'%' identified by '$DB_USER_PASSWORD';
-grant all privileges on $DB_NAME.* to '$DB_USER'@'%';
+#Check if the database exists
 
-flush privileges;
-EOF
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then 
+	echo "Database already exists"
+else
 
-echo this is endpoint!
+# Set root option so that connexion without root password is not possible
 
-exec /usr/bin/mysqld --user=mysql --console	
+mysql_secure_installation << _EOF_
+
+root4life
+root4life
+y
+n
+y
+y
+_EOF_
+
+#Add a root user on 127.0.0.1 to allow remote connexion 
+#Flush privileges allow to your sql tables to be updated automatically when you modify it
+#mysql -uroot launch mysql command line client
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+#Create database and user in the database for wordpress
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+
+#Import database in the mysql command line
+mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+
+fi
+
+/etc/init.d/mysql stop
+
+exec "$@"
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,29 +1,28 @@
-FILE_PATH="/var/www/html/index.php"
-if [ -f "$FILE_PATH" ]
+#!/bin/sh
+
+#check if wp-config.php exist
+if [ -f ./wp-config.php ]
 then
-  echo "WordPress already exists!!!"
+	echo "wordpress already downloaded"
 else
-    mv /tmp/wp-config.php /var/www/html/wp-config.php
-  
-    wp core download --allow-root --path=/var/www/html
 
-    wp	core install \
-        --allow-root \
-        --path=${WORDPRESS_PATH} \
-        --url=${WORDPRESS_URL} \
-        --title=Inception \
-        --admin_user=${WORDPRESS_ADMIN} \
-        --admin_password=${WORDPRESS_ADMIN_PASSWORD} \
-        --admin_email=${WORDPRESS_EMAIL} \
-        --skip-email
+####### MANDATORY PART ##########
 
-    wp user create  ${WORDPRESS_USER} \
-                    ${WORDPRESS_USER_EMAIL} \
-                    --user_pass=${WORDPRESS_USER_PASSWORD} \
-                    --role=author
+	#Download wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
 
-  echo "Download and set new WordPress!!"
+	#Inport env variables in the config file
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
+###################################
+
 fi
 
-
-/usr/sbin/php-fpm8 -F -R
+exec "$@"
