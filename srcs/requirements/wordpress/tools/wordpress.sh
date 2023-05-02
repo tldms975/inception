8f5c3f5e406608ea14b526edd549af662a34c06d
sielee <tldms975@naver.com>
2023-05-02 17:00:13 +0900

    s

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -19,9 +19,9 @@ wp-cli core download --allow-root
 wp-cli config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
 wp-cli core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp-cli user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=editor --allow-root
-wp-cli theme install avada --allow-root
-wp-cli theme update avada --allow-root
-wp-cli theme activate avada --allow-root
+wp-cli theme install OceanWP --allow-root
+wp-cli theme update OceanWP --allow-root
+wp-cli theme activate OceanWP --allow-root
 
 echo "wordpress ready, port is 9000"
 
